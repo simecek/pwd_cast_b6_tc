096bc4aabe3bffcddb911b236377164ea122b659
Petr Simecek <lamparna@gmail.com>
2021-09-05 16:07:44 +0200

    cm2bp fixed

--- r/utils/cm2bp.R
+++ r/utils/cm2bp.R
@@ -33,11 +33,36 @@ cm2bp_data = gm_map_data %>%
   select(Marker, chr, cM_g2f1, bp_mm10) %>%
   arrange(chr, cM_g2f1)
 
-cm2bp <- function(chrom, pos_cm) {
-  chr_data <- filter(chr == as.chromosome(chrom))
+cm2bp_onepos <- function(chrom, pos_cm) {
+  chr_data <- filter(cm2bp_data, chr == as.character(chrom))
   first_less <- max(which(chr_data$cM_g2f1 <= pos_cm))
   first_more <- min(which(chr_data$cM_g2f1 >= pos_cm))
   approx(x = chr_data$cM_g2f1[c(first_less, first_more)],
          y = chr_data$bp_mm10[c(first_less, first_more)],
          xout = pos_cm)$y
+}
+
+cm2bp <- function(chrom, pos_cm) {
+  stopifnot(length(chrom) == length(pos_cm))
+  
+  output <- rep(NA, length(chrom))
+  
+  for (c in unique(as.character(chrom))) {
+    chr_data <- filter(cm2bp_data, chr == c & !is.na(bp_mm10))
+    
+    for (i in which(chrom == c)) {
+    
+      first_less <- max(which(chr_data$cM_g2f1 <= pos_cm[i]))
+      first_more <- min(which(chr_data$cM_g2f1 >= pos_cm[i]))
+      
+      if (chr_data$cM_g2f1[first_less] == chr_data$cM_g2f1[first_more]) {
+        output[i] <- chr_data$bp_mm10[first_less]/2 + chr_data$bp_mm10[first_more]/2
+      } else {
+        output[i] <- approx(x = chr_data$cM_g2f1[c(first_less, first_more)],
+                          y = chr_data$bp_mm10[c(first_less, first_more)],
+                          xout = pos_cm[i])$y
+      }  
+    }
+  }
+  output
 }
